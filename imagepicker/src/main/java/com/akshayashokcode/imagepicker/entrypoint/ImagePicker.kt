0241b067fd2bca0b36c94b065db85c6b654cb362
Akshay Ashok <63944420+AkshayAshokCode@users.noreply.github.com>
2026-05-28 03:04:00 +0530

    Fix ImagePickerBuilder private constructor usage

--- imagepicker/src/main/java/com/akshayashokcode/imagepicker/entrypoint/ImagePicker.kt
+++ imagepicker/src/main/java/com/akshayashokcode/imagepicker/entrypoint/ImagePicker.kt
@@ -4,8 +4,15 @@ import android.content.Context
 import androidx.activity.result.ActivityResultCaller
 import com.akshayashokcode.imagepicker.builder.ImagePickerBuilder
 
+/**
+ * Public entry point for launching MediaKit image picker flows.
+ */
 object ImagePicker {
-    fun with(context: Context, caller: ActivityResultCaller): ImagePickerBuilder {
-        return ImagePickerBuilder(context, caller)
+
+    fun with(
+        context: Context,
+        caller: ActivityResultCaller
+    ): ImagePickerBuilder {
+        return ImagePickerBuilder.with(context, caller)
     }
 }
