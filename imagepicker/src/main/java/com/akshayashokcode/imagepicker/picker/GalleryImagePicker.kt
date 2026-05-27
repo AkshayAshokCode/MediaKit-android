883810063b8e22c82a89e72c346a974234900d13
Akshay Ashok <63944420+AkshayAshokCode@users.noreply.github.com>
2026-05-28 02:53:51 +0530

    Improve GalleryImagePicker documentation

--- imagepicker/src/main/java/com/akshayashokcode/imagepicker/picker/GalleryImagePicker.kt
+++ imagepicker/src/main/java/com/akshayashokcode/imagepicker/picker/GalleryImagePicker.kt
@@ -6,13 +6,24 @@ import com.akshayashokcode.imagepicker.launcher.GalleryImageLauncher
 import com.akshayashokcode.imagepicker.model.ImagePickerException
 import com.akshayashokcode.imagepicker.model.ImagePickerResult
 
+/**
+ * Gallery-backed picker implementation.
+ */
 internal class GalleryImagePicker(
     private val context: Context,
     private val caller: ActivityResultCaller,
     private val callback: (ImagePickerResult) -> Unit,
     private val onError: ((ImagePickerException) -> Unit)? = null
 ) {
-    private val launcher = GalleryImageLauncher(context, caller, callback, onError)
+
+    private val launcher by lazy {
+        GalleryImageLauncher(
+            context = context,
+            caller = caller,
+            callback = callback,
+            onError = onError
+        )
+    }
 
     fun launch() {
         launcher.launch()
